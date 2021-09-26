cbd108fb060daab009f705ba9a882c49345a54e4
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2021-09-26 22:32:28 +0700

    End generator

--- generator.py
+++ generator.py
@@ -1,20 +1,24 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as lpt
 
 
 class Generator(object):
-    def __init__(self, function, config, filename='data.csv'):
+    def __init__(self, function, config, filename='data.xlsx'):
         self.m = config[0]
-        self.n = self.m * 4
+        self.n = config[1] * 4
         self.x = self.__get_x()
-        self.u = self.__get_u(function, config[3])
-        self.y = self.__get_y(config[4])
+        self.u = self.__get_u(function, config[2])
+        self.y = self.__get_y(config[3])
         self.filename: str = filename
 
     def __get_x(self) -> type(np.ndarray):
+        res = np.ndarray((self.m, self.n))
         low, high = -1, 1
-        test = [np.random.uniform(low, high, self.n) for j in range(self.n)]
-        return np.ndarray([[np.random.uniform(low, high, self.n) for j in range(self.n)]
-                           for i in range(self.m)])
+        rows = [np.random.uniform(low, high, self.n) for i in range(self.m)]
+        for i, row in enumerate(rows):
+            res[i] = row
+        return res.transpose()
 
     def __get_u(self, function, theta) -> type(np.ndarray):
         return [function(i, theta) for i in self.x]
@@ -24,24 +28,18 @@ class Generator(object):
         u_mean = np.full(self.n, np.mean(self.u))
         w_squared = np.dot((self.u - u_mean), (self.u - u_mean)) / (self.n - 1)
         noise = coeff * w_squared
-        return np.ndarray([i + noise for i in y])
+        return y + noise
 
     def __enter__(self):
         return None
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         if self:
             self.__save(self.filename)
 
     def __save(self, filename):
-        with open(filename, 'w') as file:
-            file.write('i\t')
-            for i in range(1, self.m + 1):
-                file.write('x%d\t' % i)
-            file.write('u\ty\n')
-            for i in range(self.n):
-                file.write('{:d}\t'.format(i))
-                for j in range(self.m):
-                    file.write('{:.17f}\t'.format(self.x[i][j]))
-
-                file.write('{:.17f}\t{:.17f}\n'.format(self.u[i], self.y[i]))
+        data = pd.DataFrame()
+        for i in range(self.n):
+            current_iter = [{'X1': self.x[i][0], 'X2': self.x[i][1], 'U': self.u[i], 'Y': self.y[i]}]
+            data = data.append(current_iter, ignore_index=True)
+        data.to_excel(filename)
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from generator import Generator
 
 
 def function(x, theta):
-    return theta[0] + theta[1] * x[1] + theta[2] * x[1]**2 + \
-           theta[3] * x[2] + theta[4] * x[2]**2 + theta[5] * x[2]**3 + x[1] * x[2]
+    return theta[0] + theta[1] * x[0] + theta[2] * x[0]**2 + \
+           theta[3] * x[1] + theta[4] * x[1]**2 + theta[5] * x[1]**3 + x[0] * x[1]
 
 
 if __name__ == '__main__':
-    config = [6, [0, 0.5, 0.4, 0.3, 0.2, 0.1], 0.1]
-    with Generator(function, config):
+    config = [2, 6, [0, 0.5, 0.4, 0.3, 0.2, 0.1], 0.1]
+    with Generator(function, config, 'data.xlsx'):
         print('Generation done')
